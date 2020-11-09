7aca86a0a603b0a89d6b090724bc404d69c82e7b
Marek Ostaszewski <marek.ostaszewski@uni.lu>
2020-11-09 20:43:31 +0100

    WikiPathways fetch

--- GalaxyTools/dmaps_fetch.R
+++ GalaxyTools/dmaps_fetch.R
@@ -90,10 +90,71 @@ for(map in config[config$type == "map","resource"]) {
  
   all_maps <- c(all_maps, list(map_name = map, map_elements = model_hgncs))
    
-  message(paste0("Done."))
+  message("Done.")
 }
 
-message(paste0("Writing to JSON..."))
-cat(toJSON(all_maps), file = "out.json")
+message("Writing disease map(s) to JSON...")
+cat(toJSON(all_maps), file = "dmaps_out.json")
 
-message(paste0("Done."))
+### Massive SPARQL query (https://www.wikipathways.org/index.php/Help:WikiPathways_Sparql_queries#List_of_WikiPathways_for_HGNC_symbols)
+### with the Homo sapiens condition added
+
+sparql_url <- paste0("http://sparql.wikipathways.org/sparql?&query=select+distinct+%3FpathwayRes+str%28%3Fwpid%29+as+%3Fpathway+str%28%3Ftitle%29+as+%3FpathwayTitle+fn%3Asubstring%28%3FhgncId%2C36%29+as+%3FHGNC+where+%7B%0D%0A",
+                     "++%3Fgene+a+wp%3AGeneProduct+%3B%0D%0A",
+                     "++++dcterms%3Aidentifier+%3Fid+%3B%0D%0A",
+                     "++++dcterms%3AisPartOf+%3FpathwayRes+%3B%0D%0A",
+                     "++++wp%3AbdbHgncSymbol+%3FhgncId+.%0D%0A",
+                     "++%3FpathwayRes+a+wp%3APathway+%3B%0D%0A",
+                     "++++wp%3AorganismName+%22Homo%20sapiens%22%5E%5Exsd%3Astring+%3B%0D%0A", #Homo sapiens condition here
+                     "++++dcterms%3Aidentifier+%3Fwpid+%3B%0D%0A",
+                     "++++dc%3Atitle+%3Ftitle+.%0D%0A%7D",
+                     "&format=text%2Fcsv&timeout=0&debug=on")
+
+### Retrieve the table whole opening/closing the connection gracefully
+con <- url(sparql_url)
+res <- try(read.table(con, sep = ",", header = T, stringsAsFactors = F), 
+               silent = T)
+if(class(res) == "try-error") {
+  message(paste0("Cannot read from ", sparql_url))
+  message(res)
+  close(con)
+}
+
+### Compile a similar JSON with the WikiPathways pathways
+
+### Melt the table by the unique pathway name
+wp_list <- lapply(unique(res$pathwayTitle), function(x) list(name = x, hgncs = unique(res[res$pathwayTitle == x, "HGNC"])))
+
+message("Writing WikiPathways to JSON...")
+cat(toJSON(wp_list), file = "wikipathways.json")
+
+message("Done.")
+
+
+### Spare code ###
+
+### SPARQL query using R library; not optimal, the library is from 2013
+
+# library(SPARQL)
+# 
+# endpoint <- "http://sparql.wikipathways.org/sparql"
+# 
+# q <- "select distinct ?pathwayRes (str(?wpid) as ?pathway) (str(?title) as ?pathwayTitle) (fn:substring(?hgncId,36) as ?HGNC) where {
+#           ?gene a wp:GeneProduct ;
+#             dcterms:identifier ?id ;
+#             dcterms:isPartOf ?pathwayRes ;
+#             wp:bdbHgncSymbol ?hgncId .
+#           ?pathwayRes a wp:Pathway ;
+#             dcterms:identifier ?wpid ;
+#             dc:title ?title .
+#           ?pathway wp:organismName 'Homo sapiens^^xsd:string .
+#       }"
+# 
+# prefix <- c("wp","http://vocabularies.wikipathways.org/wp#",
+#             "rdfs","http://www.w3.org/2000/01/rdf-schema#",
+#             "dcterms","http://purl.org/dc/terms/",
+#             "xsd","http://www.w3.org/2001/XMLSchema#")
+# 
+# res <- SPARQL(endpoint,q,prefix)$results
+# 
+# 
