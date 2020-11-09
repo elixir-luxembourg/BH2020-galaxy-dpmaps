7159f70631fada3303a1f4ad385d723036aa92b6
Marek Ostaszewski <marek.ostaszewski@uni.lu>
2020-11-09 15:03:56 +0100

    Comments to the code

--- GalaxyTools/dmaps_fetch.R
+++ GalaxyTools/dmaps_fetch.R
@@ -8,6 +8,7 @@ if(length(new.packages)) install.packages(new.packages, repos='http://cran.us.r-
 library(jsonlite)
 library(httr)
 
+### A convenience function to fetch data from MINERVA
 ask_GET <- function(furl, fask) {
   resp <- httr::GET(url = paste0(furl, fask),
                     httr::add_headers('Content-Type' = "application/x-www-form-urlencoded"))
@@ -17,21 +18,33 @@ ask_GET <- function(furl, fask) {
   return(NULL)
 }
 
+### In case we want to pass parameters as an argument
 args = commandArgs(trailingOnly=TRUE)
 
+### Config listing the resources, which we want to fetch
 config <- "config.txt"
-if(length(args) > 0) { config <- args[2] }
+if(length(args) > 0) { config <- args[1] }
 if(!file.exists(config)) { message("No correct config given!")}
 config <- read.table(config, sep = ",", stringsAsFactors = F, header = T)
 
+### for a given model id (for all maps/submaps in the project)
+### Fetch HGNC symbols for proteins, RNAs and genes.
 get_hgncs <- function(fmodelid, all_models) {
+  ### MINERVA call fetching elements with relevant parameters
   fmodel <- fromJSON(ask_GET(paste0(mnv_base,"models/",fmodelid,"/"), "bioEntities/elements/?columns=id,name,type,references,bounds"), flatten = F)
+  
+  ### Model name used in naming gene sets
   model_name <- all_models[all_models$idObject == fmodelid,"name"]
+  
+  ### If no annotations, break
   if(all(is.null(unlist(fmodel$references)))) { return(NULL) }
+  
+  ### List all HGNCs in maps and submaps
   ret <- list(
     list(name = model_name, 
          hgncs = unlist(sapply(fmodel$references[fmodel$type %in% c("Protein", "RNA", "Gene")], function(x) x[x$type == "HGNC_SYMBOL", "resource"]))))
   
+  ### List all HGNCs in pathways (smaller areas within a single diagram)
   if(length(unique(fmodel[fmodel$type == "Pathway","name"])) > 0) {
     ### Calculating the inclusion of the elements per bioentity set
     for(pname in unique(me[fmodel$type == "Pathway","name"])) {
@@ -68,10 +81,11 @@ for(map in config[config$type == "map","resource"]) {
   if(is.null(models)) { next }
   models <- fromJSON(models, flatten = F)
   
-  message(paste0("Going through individual models..."))
+  message(paste0("Fetching HGNCs for individual models..."))
   
   model_hgncs <- lapply(models$idObject, get_hgncs, models)
   
+  #Remove null entries
   model_hgncs <- model_hgncs[!sapply(model_hgncs, is.null)]
  
   all_maps <- c(all_maps, list(map_name = map, map_elements = model_hgncs))
@@ -80,7 +94,6 @@ for(map in config[config$type == "map","resource"]) {
 }
 
 message(paste0("Writing to JSON..."))
-
 cat(toJSON(all_maps), file = "out.json")
 
 message(paste0("Done."))
