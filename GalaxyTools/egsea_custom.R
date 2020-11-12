241fde284b27799469a1a54fd840b0397b282f22
Marek Ostaszewski <marek.ostaszewski@uni.lu>
2020-11-12 23:38:03 +0100

    Get pathway names

--- GalaxyTools/egsea_custom.R
+++ GalaxyTools/egsea_custom.R
@@ -1,7 +1,3 @@
-library(BiocManager)
-
-BiocManager::install("EGSEA")
-
 library(EGSEA)
 
 genes <- grep("^#", readLines("input.txt"), invert = T, value = T)
@@ -10,8 +6,17 @@ library(jsonlite)
 dmaps_out <- fromJSON(paste(readLines("dmaps_out.json"), collapse = ""))
 dmaps_list <- sapply(dmaps_out[[2]], function(x) x[["hgncs"]])
 names(dmaps_list) <- sapply(dmaps_out[[2]], function(x) x[["name"]])
-dmap.gs.annot = buildCustomIdx(geneIDs = genes, gsets = dmaps_list, species="human")
-egsea.ora(geneIDs = genes, universe = unique(unlist(dmaps_list)), gs.annots = dmap.gs.annot)
+### Build a custom index for the disease map gene sets
+dmap.gs.annot <- buildCustomIdx(geneIDs = genes, gsets = dmaps_list, species="human")
+### Run egsea.ora for set-based enrichment (overrepresentation analysis), with report = F to grab the results
+egsea.result <- egsea.ora(geneIDs = genes, universe = unique(unlist(dmaps_list)), gs.annots = dmap.gs.annot, report = F)
+### Parse the results
+### 1) Grab the Experimental Contrast part of the result
+ec <- egsea.result@results$custom$test.results$ExperimentalContrast
+
+names
+
+
 
 wps_out <- fromJSON(paste(readLines("wikipathways.json"), collapse = ""))
 wps_list <- wps_out[[2]]
